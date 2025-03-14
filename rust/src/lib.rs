6af5e72322fc46971fcb4a63b14340b926371a68
dfacoet <11651258+dfacoet@users.noreply.github.com>
2025-03-14 12:47:51 +0000

    Add Rust test

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -12,3 +12,14 @@ fn _qpspin_mc(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(pi_exact_rust, m)?)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pi_exact_rust() {
+        let result = pi_exact_rust().unwrap();
+        assert_eq!(result, 3.14159265358979323846)
+    }
+}
