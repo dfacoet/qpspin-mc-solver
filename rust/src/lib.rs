d1957f84db12af8170fe2d05bf7253371f915791
dfacoet <11651258+dfacoet@users.noreply.github.com>
2025-03-14 10:35:30 +0000

    Setup maturin project for rust extension

---
+++ rust/src/lib.rs
@@ -0,0 +1,13 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn pi_exact_rust() -> PyResult<f64> {
+    let pi = 3.14159265358979323846; // more digits than f64 precision
+    Ok(pi)
+}
+
+#[pymodule]
+fn _qpspin_mc(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(pi_exact_rust, m)?)?;
+    Ok(())
+}
---
+++ src/qpspin_mc/__init__.py
@@ -0,0 +1,5 @@
+from ._qpspin_mc import pi_exact_rust as pi_exact
+
+__all__ = [
+    "pi_exact",
+]
