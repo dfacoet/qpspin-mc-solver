ebff355c26e13a80e345df142b5604a5ef65aa65
dfacoet <11651258+dfacoet@users.noreply.github.com>
2025-03-14 12:35:30 +0000

    Add Rust formatting and linting pre-commit hooks

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 
 #[pyfunction]
+#[allow(clippy::excessive_precision)]
 fn pi_exact_rust() -> PyResult<f64> {
     let pi = 3.14159265358979323846; // more digits than f64 precision
     Ok(pi)
